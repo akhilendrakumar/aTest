c43b5919d8d2e1e6fa5f15c48cd6e0efd9a87530
mister <mister@DESKTOP-534BRGJ>
2019-09-07 15:25:55 +0530

    project loadd

---
+++ akhilTest/src/test/java/com/akhilTest/Test1.java
@@ -0,0 +1,11 @@
+package com.akhilTest;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Azam Project in Akhil repo");
+
+	}
+
+}
